fe433ea77d52175cbdcaeada3780b91051140aaa
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-07-15 06:20:20 -0700

    Todo App

--- TodoList/todoApp/urls.py
+++ TodoList/todoApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+# here we have four end points to get, post, put and delete.
 urlpatterns = [
     path('', ListTodo.as_view()),
     path('<int:pk>/', DetailTodo.as_view()),
