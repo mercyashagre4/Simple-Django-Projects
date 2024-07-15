6622e03b36273123984271113c3bdd22357ba425
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-07-15 06:24:09 -0700

    Todo App

--- TodoList/todoApp/serializers.py
+++ TodoList/todoApp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import ToDo
 
+# importing serializers and serialize the objects into suitable format(json) to be utilized by any language and platform
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
