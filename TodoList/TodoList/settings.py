2a63ffd561b781199e9741cfef251e30301dc5be
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-07-15 06:18:25 -0700

    Update settings.py

--- TodoList/TodoList/settings.py
+++ TodoList/TodoList/settings.py
@@ -128,6 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# project level permission using IsAuthenticatedOrReadOnly
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
